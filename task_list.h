05b65bee440a3f02c1d058f0ddf6deb5faa2a138
Joseph Simple <113774525+Bright-Crest@users.noreply.github.com>
2023-07-01 20:08:52 +0800

    Update task_list.h 1.0
    1. add "static"
    2. add LessId, GreaterId, LessPriority, GreaterPriority
    3. modified Erase
    4. modified and add FindTask

--- task_list.h
+++ task_list.h
@@ -1,6 +1,5 @@
 #ifndef _TASK_LIST_H_
 #define _TASK_LIST_H_
-// #include <map>
 #include <ctime>
 #include <string>
 #include <utility>
@@ -11,43 +10,50 @@ using namespace std;
 
 struct Other {
   string name;
-  int begin_time;
+  time_t begin_time;
   int priority; // 4 - high, 2 - medium, 1 - low
   string type;
-  int remind_time;
+  time_t remind_time;
 };
 
-bool LessBegin(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.begin_time < task2.second.begin_time; }
-bool GreaterBegin(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.begin_time > task2.second.begin_time; }
-bool LessRemind(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.remind_time < task2.second.remind_time; }
-bool GreaterRemind(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.remind_time > task2.second.remind_time; }
+static bool LessId(pair<int, Other> task1, pair<int, Other> task2) { return task1.first < task2.first; }
+static bool GreaterId(pair<int, Other> task1, pair<int, Other> task2) { return task1.first > task2.first; }
+static bool LessBegin(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.begin_time < task2.second.begin_time; }
+static bool GreaterBegin(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.begin_time > task2.second.begin_time; }
+static bool LessRemind(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.remind_time < task2.second.remind_time; }
+static bool GreaterRemind(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.remind_time > task2.second.remind_time; }
+static bool LessPriority(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.priority < task2.second.priority; }
+static bool GreaterPriority(pair<int, Other> task1, pair<int, Other> task2) { return task1.second.priority > task2.second.priority; }
 
 class TaskList {
  public:
-  TaskList(char* filename);
-  ~TaskList();
-  bool Load(); // return false if file is doesn't exist; 
+  TaskList(char* filename); // including load
+  ~TaskList(){}
   void Add(pair<int, Other> task);
-  bool Erase(int begin_t) { return task_list_.erase(begin_t); }
-  bool Erase(string name);
+  void Erase(int id) { task_list_.erase(FindTask(id)); } 
+  void Erase(string name) { task_list_.erase(FindTask(name)); }
   void Clear() { task_list_.clear(); }
-  bool FindShow(int begin_t);
-  bool FindShow(string name);
+  bool FindShow(int id) { 
+    ShowHead();
+    ShowTask(FindTask(id)); 
+  }
+  bool FindShow(string name) {
+    ShowHead();
+    ShowTask(FindTask(name));
+  }
   void Show(int start = 0, int end = pow(2, 31) - 1, int priority_range = 7, 
             bool (*Compare)(pair<int, Other> task1, pair<int, Other> task2) = LessBegin);
   void Show(string* type_range, int start = 0, int end = pow(2, 31) - 1, int priority_range = 7,
             bool (*Compare)(pair<int, Other> task1, pair<int, Other> task2) = LessBegin);
-  bool Remind();
+  bool Remind();  // TODO Remind
 
  private:
-  // map<int, Other> task_list_; 
   vector<pair<int, Other>> task_list_;
   char* file_;
 
-  // map<int, Other>::iterator FindTask(string name);
-  vector<pair<int, Other>>::iterator FindTask(string name);
-  // void ShowTask(map<int, Other>::iterator it);
-  void ShowTask(vector<pair<int, Other>>::iterator it);
+  vector<pair<int, Other>>::iterator FindTask(int id);
+  vector<pair<int, Other>>::iterator FindTask(string name); // if not found, return task_list_.end()
+  void ShowTask(vector<pair<int, Other>>::iterator it);   // TODO ShowTask ShowHead
   void ShowHead();  // print the heading of the task list
 };
 
