b129c15b100fa4e0eae09a4e2b4a57f157f8d41a
Joseph Simple <113774525+Bright-Crest@users.noreply.github.com>
2023-07-02 17:06:13 +0800

    Update task_list.h

--- task_list.h
+++ task_list.h
@@ -28,20 +28,21 @@ static bool GreaterPriority(pair<int, Other> task1, pair<int, Other> task2) { re
 class TaskList {
  public:
   TaskList(const char* filename); // including load
-  TaskList() {}
   ~TaskList();
   bool Load(const char* filename);
-  bool Add(pair<int, Other> task);
+  // bool Add(pair<int, Other> task);
   void Erase(int id) { task_list_.erase(FindTask(id)); } 
   void Erase(string name) { task_list_.erase(FindTask(name)); }
   void Clear() { task_list_.clear(); }
   bool FindShow(int id) { 
     ShowHead();
     ShowTask(FindTask(id)); 
+    return (FindTask(id) != task_list_.end()) ? true : false;
   }
   bool FindShow(string name) {
     ShowHead();
     ShowTask(FindTask(name));
+    return (FindTask(name) != task_list_.end()) ? true : false;
   }
   void Show(int start = 0, int end = pow(2, 31) - 1, int priority_range = 7, 
             bool (*Compare)(pair<int, Other> task1, pair<int, Other> task2) = LessBegin);
@@ -59,7 +60,8 @@ class TaskList {
   void ShowTask(vector<pair<int, Other>>::iterator it);   // TODO ShowTask ShowHead
   void ShowHead();  // print the heading of the task list
 
-  void TaskList::Show(vector<pair<int, Other> > &vec, int start = 0, int end = pow(2, 31) - 1, int priority_range = 7)；
+  void Show(vector<pair<int, Other>> &vec, int start = 0,
+                      int end = pow(2, 31) - 1, int priority_range = 7);
 	//私有函数show，展示vec中begin_time介于start与end之间，priority在priority_range内的事件
   string get_priority_string(int Priority);//私有函数，根据priority的代码返回对应的优先级字符串
   void Show_with_one_priority(int start, int end, int Priority, vector<pair<int, Other> > &vec);//私有函数，处理展示一个优先级的事件的要求
