92ed4ac4f1b33a64e2a7450bc73a6d5be000cdb8
smallmall <138298455+smallmall@users.noreply.github.com>
2023-07-07 14:25:38 +0800

    Update tools.h
    增加了加载函数的定义

--- tools.h
+++ tools.h
@@ -6,3 +6,5 @@ bool isLeapYear(int year);
 bool checkTimeFormat(const std::string& inputTime);
 bool isValidDate(const std::string& inputTime);
 std::string to_string_t(time_t s);
+bool fileExists(const std::string& filename);
+void generate_txtfiles(std::string& username, std::string& password);
