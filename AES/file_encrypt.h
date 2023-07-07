a182db40ddc3f61dd84427f763a2e85eec897ad8
Joseph Simple <113774525+Bright-Crest@users.noreply.github.com>
2023-07-07 20:47:29 +0800

    Update file_encrypt.h
    Add static

--- AES/file_encrypt.h
+++ AES/file_encrypt.h
@@ -8,7 +8,7 @@ using namespace std;
 
 void EncryptFile(const string& filename, const string& en_filename, string key);
 void DecryptFile(const string& filename, const string& de_filename, string key); 
-void File2VectorUnCh(const string& filename, vector<unsigned char>& data);
-void VectorUnCh2BinFile(const vector<unsigned char>& data, const string& filename);
-void BinFile2VectorUnCh(const string& filename, vector<unsigned char>& data);
-void VectorUnCh2File(const vector<unsigned char>& data, const string& filename);
+static void File2VectorUnCh(const string& filename, vector<unsigned char>& data);
+static void VectorUnCh2BinFile(const vector<unsigned char>& data, const string& filename);
+static void BinFile2VectorUnCh(const string& filename, vector<unsigned char>& data);
+static void VectorUnCh2File(const vector<unsigned char>& data, const string& filename);
