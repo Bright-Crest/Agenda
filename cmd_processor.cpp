505b6a3c73972e8f291ef2c9fcff66056ce4ed81
Joseph Simple <113774525+Bright-Crest@users.noreply.github.com>
2023-07-10 16:19:04 +0800

    Update cmd_processor.cpp

--- cmd_processor.cpp
+++ cmd_processor.cpp
@@ -110,7 +110,7 @@ int CmdProcessor::CmdDistributor(TaskList& task_list) const {
 // ",选项的顺序可以打乱)
 bool AddTaskOp(TaskList& task_list, list<string> cmd) {
   Other task;
-  task.priority = 0;
+  task.priority = 1;
   task.type = "-";
   string begin;  // begin用于保存begin_time_并分配唯一的id
   list<string>::iterator it = ++cmd.begin();  // 从cmd的第二个string开始参考
